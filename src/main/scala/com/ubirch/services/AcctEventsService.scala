1c93ca21acbb4441a9673850f68f04fda070762f
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-18 22:18:43 +0100

    UC-165 Some refactorings

--- src/main/scala/com/ubirch/services/AcctEventsService.scala
+++ src/main/scala/com/ubirch/services/AcctEventsService.scala
@@ -23,70 +23,45 @@ trait AcctEventsService {
 
 @Singleton
 class DefaultAcctEventsService @Inject() (acctEventDAO: AcctEventDAO, acctEventByCatDAO: AcctEventByCatDAO) extends AcctEventsService with LazyLogging {
+
   override def byOwnerIdAndIdentityId(ownerId: UUID, category: Option[String], identityId: Option[UUID], start: Option[LocalDate], end: Option[LocalDate]): Observable[AcctEventRow] = {
 
-    category match {
-      case Some(cat) =>
-
-        identityId match {
-          case Some(id) =>
-            (start, end) match {
-              case (Some(s), Some(e)) =>
-
-                acctEventByCatDAO.byOwnerIdAndIdentityId(ownerId, cat, id, s, e)
-                  .onErrorHandle { e =>
-                    logger.error("error_cat_byOwnerIdAndIdentityId -> ", e)
-                    throw e
-                  }
-
-              case _ =>
-
-                acctEventByCatDAO.byOwnerIdAndIdentityId(ownerId, cat, id)
-                  .onErrorHandle { e =>
-                    logger.error("error_cat_byOwnerIdAndIdentityId -> ", e)
-                    throw e
-                  }
-            }
-
-          case None =>
-            acctEventByCatDAO.byOwnerId(ownerId, cat)
-              .onErrorHandle { e =>
-                logger.error("error_cat_byOwnerId -> ", e)
-                throw e
-              }
-        }
-
-      case None =>
-        identityId match {
-          case Some(id) =>
-            (start, end) match {
-              case (Some(s), Some(e)) =>
-
-                acctEventDAO.byOwnerIdAndIdentityId(ownerId, id, s, e)
-                  .onErrorHandle { e =>
-                    logger.error("error_byOwnerIdAndIdentityId -> ", e)
-                    throw e
-                  }
-
-              case _ =>
-
-                acctEventDAO.byOwnerIdAndIdentityId(ownerId, id)
-                  .onErrorHandle { e =>
-                    logger.error("error_byOwnerIdAndIdentityId -> ", e)
-                    throw e
-                  }
-            }
-
-          case None => acctEventDAO.byOwnerId(ownerId).onErrorHandle { e =>
-            logger.error("error_byOwnerId -> ", e)
-            throw e
-          }
-        }
-    }
+    byOwnerIdAndIdentityIdBase(ownerId, category, identityId, start, end)(
+      a = (ownerId, cat, id, s, e) => acctEventByCatDAO.byOwnerIdAndIdentityId(ownerId, cat, id, s, e),
+      b = (ownerId, cat, id) => acctEventByCatDAO.byOwnerIdAndIdentityId(ownerId, cat, id),
+      c = (ownerId, cat) => acctEventByCatDAO.byOwnerId(ownerId, cat),
+      d = (ownerId, id, s, e) => acctEventDAO.byOwnerIdAndIdentityId(ownerId, id, s, e),
+      e = (ownerId, id) => acctEventDAO.byOwnerIdAndIdentityId(ownerId, id),
+      f = ownerId => acctEventDAO.byOwnerId(ownerId)
+    )
 
   }
 
   override def byOwnerIdAndIdentityIdCount(ownerId: UUID, category: Option[String], identityId: Option[UUID], start: Option[LocalDate], end: Option[LocalDate]): Observable[Long] = {
+    byOwnerIdAndIdentityIdBase(ownerId, category, identityId, start, end)(
+      a = (ownerId, cat, id, s, e) => acctEventByCatDAO.byOwnerIdAndIdentityIdCount(ownerId, cat, id, s, e),
+      b = (ownerId, cat, id) => acctEventByCatDAO.byOwnerIdAndIdentityIdCount(ownerId, cat, id),
+      c = (ownerId, cat) => acctEventByCatDAO.byOwnerIdCount(ownerId, cat),
+      d = (ownerId, id, s, e) => acctEventDAO.byOwnerIdAndIdentityIdCount(ownerId, id, s, e),
+      e = (ownerId, id) => acctEventDAO.byOwnerIdAndIdentityIdCount(ownerId, id),
+      f = ownerId => acctEventDAO.byOwnerIdCount(ownerId)
+    )
+  }
+
+  private def byOwnerIdAndIdentityIdBase[T](
+      ownerId: UUID,
+      category: Option[String],
+      identityId: Option[UUID],
+      start: Option[LocalDate],
+      end: Option[LocalDate]
+  )(
+      a: (UUID, String, UUID, LocalDate, LocalDate) => Observable[T],
+      b: (UUID, String, UUID) => Observable[T],
+      c: (UUID, String) => Observable[T],
+      d: (UUID, UUID, LocalDate, LocalDate) => Observable[T],
+      e: (UUID, UUID) => Observable[T],
+      f: (UUID) => Observable[T]
+  ): Observable[T] = {
 
     category match {
       case Some(cat) =>
@@ -96,25 +71,31 @@ class DefaultAcctEventsService @Inject() (acctEventDAO: AcctEventDAO, acctEventB
             (start, end) match {
               case (Some(s), Some(e)) =>
 
-                acctEventByCatDAO.byOwnerIdAndIdentityIdCount(ownerId, cat, id, s, e)
+                a(ownerId, cat, id, s, e)
                   .onErrorHandle { e =>
-                    logger.error("error_cat_count_byOwnerIdAndIdentityId -> ", e)
+                    logger.error("error_cat_a -> ", e)
+                    throw e
+                  }
+
+              case (None, None) =>
+
+                b(ownerId, cat, id)
+                  .onErrorHandle { e =>
+                    logger.error("error_cat_b -> ", e)
                     throw e
                   }
 
               case _ =>
-
-                acctEventByCatDAO.byOwnerIdAndIdentityIdCount(ownerId, cat, id)
-                  .onErrorHandle { e =>
-                    logger.error("error_cat_count_byOwnerIdAndIdentityIdCount -> ", e)
-                    throw e
-                  }
+                Observable.raiseError {
+                  logger.error("error -> ", e)
+                  new IllegalArgumentException("Start and End must be present")
+                }
             }
 
           case None =>
-            acctEventByCatDAO.byOwnerIdCount(ownerId, cat)
+            c(ownerId, cat)
               .onErrorHandle { e =>
-                logger.error("error_cat_count_byOwnerIdCount -> ", e)
+                logger.error("error_cat_c -> ", e)
                 throw e
               }
         }
@@ -125,28 +106,35 @@ class DefaultAcctEventsService @Inject() (acctEventDAO: AcctEventDAO, acctEventB
             (start, end) match {
               case (Some(s), Some(e)) =>
 
-                acctEventDAO.byOwnerIdAndIdentityIdCount(ownerId, id, s, e)
+                d(ownerId, id, s, e)
                   .onErrorHandle { e =>
-                    logger.error("error_cat_count_byOwnerIdAndIdentityIdCount -> ", e)
+                    logger.error("error_d -> ", e)
+                    throw e
+                  }
+
+              case (None, None) =>
+
+                e(ownerId, id)
+                  .onErrorHandle { e =>
+                    logger.error("error_e -> ", e)
                     throw e
                   }
 
               case _ =>
-
-                acctEventDAO.byOwnerIdAndIdentityIdCount(ownerId, id)
-                  .onErrorHandle { e =>
-                    logger.error("error_cat_count_byOwnerIdAndIdentityIdCount -> ", e)
-                    throw e
-                  }
+                Observable.raiseError {
+                  logger.error("error -> ", e)
+                  new IllegalArgumentException("Start and End must be present")
+                }
             }
 
           case None =>
-            acctEventDAO.byOwnerIdCount(ownerId)
+            f(ownerId)
               .onErrorHandle { e =>
-                logger.error("error_cat_count_byOwnerIdCount -> ", e)
+                logger.error("error_f-> ", e)
                 throw e
               }
         }
     }
+
   }
 }
