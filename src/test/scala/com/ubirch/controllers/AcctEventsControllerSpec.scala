ab68b0cfbaff5d0002a851f495206fbc194812fe
Carlos Sanchez <caaugustoss@gmail.com>
2022-02-10 11:15:32 +0100

    UC-165 Model design V4 - Adding test

--- src/test/scala/com/ubirch/controllers/AcctEventsControllerSpec.scala
+++ src/test/scala/com/ubirch/controllers/AcctEventsControllerSpec.scala
@@ -3,6 +3,7 @@ package com.ubirch.controllers
 import com.ubirch._
 import com.ubirch.services.jwt.PublicKeyPoolService
 
+import com.github.nosan.embedded.cassandra.api.cql.CqlScript
 import io.prometheus.client.CollectorRegistry
 import org.scalatest.BeforeAndAfterEach
 import org.scalatra.test.scalatest.ScalatraWordSpec
@@ -26,6 +27,50 @@ class AcctEventsControllerSpec
   private lazy val Injector = new InjectorHelperImpl() {}
 
   "Acct Events Service" must {
+
+    "get OK with result with date" in {
+      List(
+        CqlScript.ofString(
+          "INSERT INTO acct_system.acct_events (identity_id, category, year, month, day, hour, sub_category, id, occurred_at) " +
+          "VALUES (12539f76-c7e9-47d6-b37b-4b59380721ac, 'verification', 2022, 2, 9, 11, 'entry-b', 000014a4-02f0-4c69-96f0-d85de7cb9dd8, '2022-02-09 11:45:43.401');"
+        ),
+        CqlScript.ofString(
+          "INSERT INTO acct_system.acct_events (identity_id, category, year, month, day, hour, sub_category, id, occurred_at) " +
+            "VALUES (12539f76-c7e9-47d6-b37b-4b59380721ac, 'verification', 2022, 2, 9, 12, 'entry-a', 000014a4-02f0-4c69-96f0-d85de7cb9dd8, '2022-02-09 12:45:43.401');"
+        ),
+      ).foreach(x => x.forEachStatement { x => val _ = cassandra.connection.execute(x) })
+
+      val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJodHRwczovL3Rva2VuLmRldi51YmlyY2guY29tIiwic3ViIjoiOTYzOTk1ZWQtY2UxMi00ZWE1LTg5ZGMtYjE4MTcwMWQxZDdiIiwiYXVkIjpbImh0dHBzOi8vYXBpLmNvbnNvbGUuZGV2LnViaXJjaC5jb20iLCJodHRwczovL2RhdGEuZGV2LnViaXJjaC5jb20iXSwiaWF0IjoxNjQ0NDg1OTA5LCJqdGkiOiI0NmZkYzczNi0zOWJjLTQ1NDUtYWVhNi1kZTgzNjBhYjJmNWYiLCJzY3AiOlsidGhpbmc6Z2V0aW5mbyIsInRoaW5nOnN0b3JlZGF0YSJdLCJwdXIiOiJBY2NvdW50aW5nIFNlcnZpY2UiLCJ0Z3AiOltdLCJ0aWQiOlsiMTI1MzlmNzYtYzdlOS00N2Q2LWIzN2ItNGI1OTM4MDcyMWFjIl0sIm9yZCI6W119.AE1mMNKiq9j9P-_U0kan7Vi3hW7dRVs-aQ-nFRMqNEheTOdQ4RDKx7CmpsbdoBoo8koN2TrRVkEXQLr7X1zgLg"
+
+      def identity = "12539f76-c7e9-47d6-b37b-4b59380721ac"
+      def date = "2022-02-01"
+      val category = "verification"
+
+      var subcategory = "entry-b"
+
+      get(s"/v1/$identity?date=$date&cat=$category&sub_cat=$subcategory", headers = Map("authorization" -> s"bearer $token")) {
+        status should equal(200)
+        val expected = """{"version":"1.0.0","ok":true,"data":[{"year":2022,"month":2,"count":1}]}""".stripMargin
+        assert(body == expected)
+      }
+
+      subcategory = "entry-a"
+
+      get(s"/v1/$identity?date=$date&cat=$category&sub_cat=$subcategory", headers = Map("authorization" -> s"bearer $token")) {
+        status should equal(200)
+        val expected = """{"version":"1.0.0","ok":true,"data":[{"year":2022,"month":2,"count":1}]}""".stripMargin
+        assert(body == expected)
+      }
+
+      // All subcats
+
+      get(s"/v1/$identity?date=$date&cat=$category", headers = Map("authorization" -> s"bearer $token")) {
+        status should equal(200)
+        val expected = """{"version":"1.0.0","ok":true,"data":[{"year":2022,"month":2,"count":2}]}""".stripMargin
+        assert(body == expected)
+      }
+
+    }
 
     "fail when no access token provided: get" in {
 
