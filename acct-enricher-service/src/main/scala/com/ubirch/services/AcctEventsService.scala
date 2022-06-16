627cc223f0ee92b650e503c9349e7a02d28500f1
Carlos Sanchez <caaugustoss@gmail.com>
2022-06-16 12:54:47 +0200

    UC-334 Improving aggregation

--- acct-enricher-service/src/main/scala/com/ubirch/services/AcctEventsService.scala
+++ acct-enricher-service/src/main/scala/com/ubirch/services/AcctEventsService.scala
@@ -1,9 +1,8 @@
 package com.ubirch.services
 
-import com.ubirch.models.cassandra.{ AcctEventOwnerRow, AcctEventRow, AcctStoreDAO }
+import com.ubirch.models.cassandra.AcctStoreDAO
 
 import monix.eval.Task
-import monix.reactive.Observable
 
 import java.time.LocalDate
 import java.util.UUID
@@ -17,27 +16,12 @@ trait AcctEventsService {
       date: LocalDate,
       subCategory: Option[String]
   ): Task[MonthlyCountResult]
-
-  def getKnownIdentitiesByOwner(ownerId: UUID): Observable[AcctEventOwnerRow]
-
-  def byTime(
-      identityId: UUID,
-      category: String,
-      date: LocalDate,
-      subCategory: Option[String]
-  ): Observable[AcctEventRow]
 }
 
 case class MonthlyCountResult(identityId: UUID, tenantId: UUID, category: String, date: LocalDate, subCategory: Option[String], year: Int, month: Int, count: Long)
 
 @Singleton
 class DefaultAcctEventsService @Inject() (acctStoreDAO: AcctStoreDAO) extends AcctEventsService {
-
-  def monthRange(date: LocalDate): Seq[(Int, Int)] = (1 to date.lengthOfMonth).flatMap { day =>
-    (0 to 23).map { hour =>
-      (day, hour)
-    }
-  }
 
   override def monthCount(identityId: UUID, tenantId: UUID, category: String, date: LocalDate, subCategory: Option[String]): Task[MonthlyCountResult] = {
 
@@ -70,23 +54,10 @@ class DefaultAcctEventsService @Inject() (acctStoreDAO: AcctStoreDAO) extends Ac
 
   }
 
-  override def getKnownIdentitiesByOwner(ownerId: UUID): Observable[AcctEventOwnerRow] = {
-    acctStoreDAO.owner.byOwnerId(ownerId)
-  }
-
-  override def byTime(identityId: UUID, category: String, date: LocalDate, subCategory: Option[String]): Observable[AcctEventRow] = {
-
-    Observable.fromIterable(monthRange(date)).flatMap { case (day, hour) =>
-      acctStoreDAO.events.byTime(
-        identityId = identityId,
-        category = category,
-        year = date.getYear,
-        month = date.getMonthValue,
-        day = day,
-        hour = hour,
-        subCategory = subCategory
-      )
+  def monthRange(date: LocalDate): Seq[(Int, Int)] = (1 to date.lengthOfMonth).flatMap { day =>
+    (0 to 23).map { hour =>
+      (day, hour)
     }
-
   }
+
 }
